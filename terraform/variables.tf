de5750ae270bae264151508d4dc25762b9f5d379
mingjiewong <massivecavern@hotmail.com>
2025-01-31 20:55:09 +0800

    Fix: update ec2 instance type

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,7 +20,7 @@ variable "ami_id" {
 variable "ec2_instance_type" {
   description = "The EC2 instance type"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.large"
 }
 
 # SSH key pair name for EC2 instance access
